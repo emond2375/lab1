ab3c16adcf1836c9bb2cea2609d334533807189e
Utilisateur P2-1020 <p2-1020@laboratoire.uqac.ca>
2018-09-04 18:31:22 -0400

    V4

--- mon_projet.py
+++ mon_projet.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=None):
+    return render_template('accueil.tpl', name=name)
